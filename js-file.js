f76f34dfb8e064af43f4dd839f9cf1d78e583bd7
Alpha-Dolphi <djedai@list.ru>
2022-09-03 20:25:04 +0300

    adding a footer and a changing background feature

--- js-file.js
+++ js-file.js
@@ -9,22 +9,27 @@
     const eraser = document.querySelector('#eraser');
     const colorMode = document.querySelector('#color-mode');
     const rainbowMode = document.querySelector('#rainbow-mode');
+    const back = document.querySelector('#back');
+    const frame = document.querySelector('#frame');
+
+    document.querySelector('#back').value = "#FFFFFF";
 
     // Setting up an initial color and canvas scale
-    let brushColor = "black";
-    rainbow = false;
-    var first = 0;
+    let brushColor = "black", rainbow = false, first = 0, prevBackColor = `rgb(${hexToRgb('#FFFFFF').r}, ${hexToRgb('#FFFFFF').g}, ${hexToRgb("#FFFFFF").b})`;;
     if(first === 0) {
         first++;
-        filling(16);
+        filling(16, "#FFFFFF");
     }
 
-    function filling(value) {        
+    function filling(value, backColor) {        
         canvas.textContent = ''; // Emptying the canvas     
         let fragment = new DocumentFragment(); 
         for (var i = 0; i<value*value; i++) { // Filling the fragment with div elements
             var div = document.createElement('div');
             div.classList.add("cell");  
+            div.style.value = "clean";
+            div.style.backgroundColor = backColor;
+            // alert(document.querySelector('.cell').style.backgroundColor);
             var width = height = (450/value);     
             div.style.width = `${width}px`;
             div.style.height = `${height}px`;
@@ -48,9 +53,11 @@
                 if (rainbow === true) {
                     // generating a random color
                     cell.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16); 
+                    cell.style.value = "painted";
                 }  
                 else {                    
                     cell.style.backgroundColor = brushColor;    
+                    cell.style.value = "painted";
                 }     
             });            
             // checking what happens to a mouse outside of the canvas
@@ -64,9 +71,11 @@
                     if (rainbow === true) {
                         // generating a random color
                         cell.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+                        cell.style.value = "painted";
                     }  
                     else {
                         cell.style.backgroundColor = brushColor;    
+                        cell.style.value = "painted";
                     }                                       
                 }                                              
             });                                     
@@ -107,12 +116,12 @@
     })
 
     clear.addEventListener("click", () => {
-        filling(range.value);
+        filling(range.value, document.querySelector('#back').value);
     })
     
     eraser.addEventListener("click", () => {
         rainbow = false;
-        brushColor = "white";
+        brushColor = document.querySelector('#back').value;
     })
 
     rainbowMode.addEventListener("click", () => {
@@ -124,13 +133,49 @@
         brushColor = colorInput.value;
     })
 
-    background.addEventListener("click", () => {
-        
+    function changeBackground(back) {
+        let scale = document.querySelector('#range-input').value;
+        const cells = document.querySelectorAll('.cell');   
+        cells.forEach((cell) => {
+            if (cell.style.backgroundColor == prevBackColor && cell.style.value == "clean") {
+                // console.log("cell.style.backgroundColor = "+ cell.style.backgroundColor);
+                // console.log("prevBackColor = " + prevBackColor);
+                cell.style.backgroundColor = back;
+            }
+        });      
+        // alert(back);
+        prevBackColor = `rgb(${hexToRgb(back).r}, ${hexToRgb(back).g}, ${hexToRgb(back).b})`;
+        // alert(prevBackColor);
+        // painting();
+        // filling(scale, back);
+    }
+
+    frame.addEventListener("click", () => {
+        back.click();
     })
 
     // dynamically showing the scale of the canvas
     function showScale(value) {
         scale.textContent = `${value}x${value}`;  
     }
+
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+      }
+      
+      function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+      }
+
+      function hexToRgb(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16)
+        } : null;
+    }
+
 
     
